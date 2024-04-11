28d14f0935bc81f452357e7ba3bceeba25bdc9d5
vinicius.paixao <vinicius.paixao@peopleinteractive.com.br>
2024-04-11 13:31:19 -0300

    feat(ocultaçao): ocultaçao de serviçoes em progresso

--- src/plugins/sales-channels/admin/src/index.tsx
+++ src/plugins/sales-channels/admin/src/index.tsx
@@ -9,34 +9,34 @@ const name = pluginPkg.strapi.name;
 
 export default {
   register(app: any) {
-    app.addMenuLink({
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
-      },
-      Component: async () => {
-        const component = await import('./pages/App');
+    // app.addMenuLink({
+    //   to: `/plugins/${pluginId}`,
+    //   icon: PluginIcon,
+    //   intlLabel: {
+    //     id: `${pluginId}.plugin.name`,
+    //     defaultMessage: name,
+    //   },
+    //   Component: async () => {
+    //     const component = await import('./pages/App');
 
-        return component;
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
-    });
-    const plugin = {
-      id: pluginId,
-      initializer: Initializer,
-      isReady: false,
-      name,
-    };
+    //     return component;
+    //   },
+    //   permissions: [
+    //     // Uncomment to set the permissions of the plugin here
+    //     // {
+    //     //   action: '', // the action name should be plugin::plugin-name.actionType
+    //     //   subject: null,
+    //     // },
+    //   ],
+    // });
+    // const plugin = {
+    //   id: pluginId,
+    //   initializer: Initializer,
+    //   isReady: false,
+    //   name,
+    // };
 
-    app.registerPlugin(plugin);
+    // app.registerPlugin(plugin);
   },
 
   bootstrap(app: any) {},
